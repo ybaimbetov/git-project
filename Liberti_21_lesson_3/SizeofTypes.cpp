4c012b59495e7eadbbc3cbcff32ee9261c4bd316
yerdosollie <yerdosollie@gmail.com>
2017-09-22 12:04:41 +0600

    comment

--- Liberti_21_lesson_3/SizeofTypes.cpp
+++ Liberti_21_lesson_3/SizeofTypes.cpp
@@ -13,6 +13,14 @@ int main()
   cout << "The size of a double is : \t\t" << sizeof(double) << " bytes.\n";
   cout << "The size of a bool is : \t\t" << sizeof(bool) << " bytes.\n";
 
+  cout << "The size of an int is : \t\t" << sizeof(int) << " bytes.\n";
+  cout << "The size of a short int is : \t\t" << sizeof(short) << " bytes.\n";
+  cout << "The size of a long int is : \t\t" << sizeof(long) << " bytes.\n";
+  cout << "The size of a char is : \t\t" << sizeof(char) << " bytes.\n";
+  cout << "The size of a float is : \t\t" << sizeof(float) << " bytes.\n";
+  cout << "The size of a double is : \t\t" << sizeof(double) << " bytes.\n";
+  cout << "The size of a bool is : \t\t" << sizeof(bool) << " bytes.\n";
+  
   _getch();
   return 0;
   }
