75d07c8ae63da0cded99247ffbeb1ab986cd3ab8
yerdosollie <yerdosollie@gmail.com>
2017-09-22 12:16:29 +0600

    Comment with Nodepad++

--- Liberti_21_lesson_3/SizeofTypes.cpp
+++ Liberti_21_lesson_3/SizeofTypes.cpp
@@ -21,6 +21,11 @@ int main()
   cout << "The size of a double is : \t\t" << sizeof(double) << " bytes.\n";
   cout << "The size of a bool is : \t\t" << sizeof(bool) << " bytes.\n";
   
+  
+  cout << "The size of a float is : \t\t" << sizeof(float) << " bytes.\n";
+  cout << "The size of a double is : \t\t" << sizeof(double) << " bytes.\n";
+  cout << "The size of a bool is : \t\t" << sizeof(bool) << " bytes.\n";
+ 
   _getch();
   return 0;
   }
