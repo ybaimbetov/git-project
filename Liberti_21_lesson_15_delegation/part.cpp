820741559eeff29a9384d5891e6391dade61beb9
yerdosollie <yerdosollie@gmail.com>
2017-09-26 18:03:14 +0600

    github

--- Liberti_21_lesson_15_delegation/part.cpp
+++ Liberti_21_lesson_15_delegation/part.cpp
@@ -6,6 +6,6 @@ using namespace std;
 void Part::Display() const
   {
   cout << "\nPart Number: " << m_nPartNumber << endl;
-  cout << "\nPart Number+Number: " << m_nPartNumber + m_nPartNumber << endl;
+  //cout << "\nPart Number+Number: " << m_nPartNumber + m_nPartNumber << endl;
   }
 
